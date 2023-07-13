46d876bed89afeebded250cd3c93a8c80a699683
abrorm3 <abrormukhammadiev@gmail.com>
2023-07-14 00:31:39 +0500

    fixed environment error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { CardsComponent } from './main/cards/cards.component';
 import { MainCenterComponent } from './main/main-center/main-center.component';
@@ -13,11 +16,12 @@ import { SecSubscriptionsComponent } from './main/main-center/sec-subscriptions/
 import { SecObligatoryComponent } from './main/main-center/sec-obligatory/sec-obligatory.component';
 import { SecStatisticComponent } from './main/main-center/sec-statistic/sec-statistic.component';
 import { SecAdminComponent } from './main/main-center/sec-admin/sec-admin.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TrEditComponent } from './main/sidebar/tr-edit/tr-edit.component';
 import { AuthModule } from './auth/auth.module';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -40,11 +44,21 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     FormsModule,
     MatSidenavModule,
     BrowserAnimationsModule,
-    AuthModule
+    AuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
-  providers:[
-    [{provide:HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi:true}]
+
+  providers: [
+    [
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService,
+        multi: true,
+      },
+    ],
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCp91xKJKazfb-pG81eJUkUWmLvFpZiw9c",
+    authDomain: "wallet-tracker-57bf6.firebaseapp.com",
+    databaseURL: "https://wallet-tracker-57bf6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wallet-tracker-57bf6",
+    storageBucket: "wallet-tracker-57bf6.appspot.com",
+    messagingSenderId: "1079256754047",
+    appId: "1:1079256754047:web:76d00a2c0120c82adc5c4a",
+    measurementId: "G-M7WMLHDNZQ"
+  }
+};
